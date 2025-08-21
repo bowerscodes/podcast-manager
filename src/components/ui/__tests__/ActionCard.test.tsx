676f246521c2248f746b1bdf731b9e5704f4611e
James Bowers <jamesbowers123@gmail.com>
2025-08-21 12:02:42 +0100

    relocate ActionCard component

--- src/components/ui/__tests__/ActionCard.test.tsx
+++ src/components/ui/__tests__/ActionCard.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import ActionCard from '../../ActionCard';
+import ActionCard from '../ActionCard';
 
 // Mock Next.js router
 const mockPush = jest.fn();
